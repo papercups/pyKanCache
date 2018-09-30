b15c4427634dc3ee6557df6ef93d7f117474066d
papercups <papercups@vip.qq.com>
2018-10-01 01:48:43 +0800

    v0.1 本地缓存实现

---
+++ cache.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+import json
+import time
+from contextlib import closing
+
+
+CACHE_PATH = os.path.join(os.getcwd(), 'cache')
+CHECK_JSON_PATH = os.path.join(CACHE_PATH, 'check.json')
+
+# 初始化缓存文件夹和校验用json
+if not os.path.isdir(CACHE_PATH):
+    os.mkdir(CACHE_PATH)
+
+if os.path.isfile(CHECK_JSON_PATH):
+    with open(CHECK_JSON_PATH, 'r') as fp:
+        check_json = json.load(fp)
+else:
+    with open(CHECK_JSON_PATH, 'w') as fp:
+        fp.write('{}')
+    check_json = dict()
+
+
+# 检测缓存有效性
+def checkCache(cache_path, version):
+    '''
+    args:
+        cache_path: 缓存文件相对路径
+        version: 缓存版本号,没有时为None
+
+    return:
+        0: 可以使用
+        1: 无效或不存在,要下载
+        文件修改时间: 过期,需要向服务器验证
+    '''
+
+    if cache_path not in check_json:
+        return 1
+
+    # 带version的先检查version
+    if version:
+        # 不一致的的话就要重新下载
+        if version != check_json[cache_path]['version']:
+            return 1
+
+    file_path = os.path.join(CACHE_PATH, cache_path)
+    # 检查文件是否存在
+    if not os.path.isfile(file_path):
+        return 1
+
+    # 检查是否过期
+    deadline = float(check_json[cache_path]['deadline'])
+    # 过期要用If-Modified-Since检测
+    if deadline < time.time():
+        # 返回修改时间,肯定大于1
+        return os.path.getmtime(file_path)
+
+    # 三个检查都通过的可以使用
+    return 0
+
+
+def getCache(cache_path):
+    file_name = os.path.join(CACHE_PATH, cache_path)
+    cache_byte = b''
+    with closing(
+        open(file_name, 'rb')
+    ) as fp:
+        cache_byte = fp.read()
+        return cache_byte
+
+def setCache(cache_byte, cache_path, cache_json):
+    file_path, file_name = os.path.split(cache_path)
+    file_path = os.path.join(CACHE_PATH, file_path)
+    file_name = os.path.join(file_path, file_name)
+    if not os.path.isdir(file_path):
+        try:
+            os.makedirs(file_path)
+        except:
+            pass
+
+    with closing(
+        open(file_name, 'wb')
+    ) as fp:
+        fp.write(cache_byte)
+
+    check_json.update({cache_path:cache_json})
+    with closing(
+        open(CHECK_JSON_PATH, 'w')
+    ) as fp:
+        json.dump(check_json, fp)
---
+++ server.py
@@ -0,0 +1,148 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from contextlib import closing
+import os
+import time
+
+from flask import Flask, request, Response
+import requests
+
+import cache
+
+
+# 响应头
+cache_headers = {
+    'Server': 'nginx',
+    #'Pragma': 'public',
+    'Accept-Ranges': 'byte',
+}
+# 不同类型文件的Content-Type
+Content_Types = {
+    '.png': {'Content-Type': 'image/png'},
+    '.mp3': {'Content-Type': 'audio/mpeg'},
+    '.json': {'Content-Type': 'application/json'},
+    '.css': {'Content-Type': 'text/css'},
+}
+
+app = Flask('pyKanCache')
+
+
+# 代理GET请求
+@app.route('/<path:subpath>', methods = ['GET'])
+def proxyGET(subpath):
+    # 用扩展名判断是否使用本地缓存
+    cache_type = os.path.splitext(subpath)[1]
+    if cache_type in ('.png', '.mp3'):
+        # 先检测缓存是否有效
+        cache_flag = cache.checkCache(
+            subpath,
+            request.args.get('version', default = None)
+        )
+        if cache_flag != 0:
+            # 带着cache_flag进download,需要验证时就带If-Modified-Since
+            resp_cache = download(request, cache_flag)
+        # 缓存有效或验证后返回304时用本地缓存
+        if cache_flag == 0 or resp_cache.status_code == 304:
+            app.logger.debug('使用本地缓存: %s', subpath)
+            cache_byte = cache.getCache(subpath)
+            # 用扩展名完成headers
+            cache_headers.update(Content_Types[cache_type])
+            cache_headers['Content-Length'] = len(cache_byte)
+            return Response(cache_byte, 200, cache_headers)
+        else:
+            cache_json = {
+                'deadline': str(time.time() + 2592000),
+                'version': request.args.get('version', default = None),
+            }
+            cache.setCache(resp_cache.data, subpath, cache_json)
+            return resp_cache
+
+    else:
+        app.logger.debug('转发GET: %s', request.url)
+        return transmitGET(request)
+
+@app.route('/<path:subpath>', methods = ['POST'])
+def proxyPOST(subpath):
+    resp = transmitPOST(request)
+    if resp.status_code == 201:
+        app.logger.warning('错误代码: 201')
+    return resp
+
+def transmitPOST(request):
+    '''直接转发POST请求
+    '''
+
+    url = request.url
+    data = request.form or None
+    headers = dict(request.headers)
+
+    with closing(
+        requests.post(
+            url, headers = headers, data = data)
+    ) as r:
+        headers = dict(r.headers)
+        if headers.get('Content-Encoding') and \
+           headers['Content-Encoding'] in ('gzip', 'deflate'):
+            del headers['Content-Encoding']
+        if headers.get('Transfer-Encoding'):
+            del headers['Transfer-Encoding']
+        headers['Content-Length'] = len(r.content)
+
+        return Response(r.content, r.status_code, headers)
+
+def transmitGET(request):
+    '''直接转发GET请求
+    '''
+
+    url = request.url
+    data = request.data or None
+    headers = dict()
+    for key, value in request.headers:
+        if not value or key == 'Cache-Control':
+            continue
+        headers[key] = value
+
+    with closing(
+        requests.get(
+            url, headers = headers, data = data, stream = True)
+    ) as r:
+        headers = dict(r.headers)
+        if headers.get('Content-Encoding') and \
+           headers['Content-Encoding'] in ('gzip', 'deflate'):
+            del headers['Content-Encoding']
+        if headers.get('Transfer-Encoding'):
+            del headers['Transfer-Encoding']
+        headers['Content-Length'] = len(r.content)
+
+        return Response(r.content, r.status_code, headers)
+
+# 下载缓存文件
+def download(request, cache_flag):
+    url = request.url
+    data = request.data or None
+    headers = dict()
+    for key, value in request.headers:
+        if not value or key == 'Cache-Control':
+            continue
+        headers[key] = value
+    if cache_flag > 1:
+        # 时间戳格式转服务器时间戳格式
+        headers['If-Modified-Since'] = \
+            time.strftime('%a, %d %b %Y %H:%M:%S GMT', cache_flag)
+
+    with closing(
+        requests.get(
+            url, headers = headers, data = data, stream = True)
+    ) as r:
+        headers = dict(r.headers)
+        if headers.get('Content-Encoding') and \
+           headers['Content-Encoding'] in ('gzip', 'deflate'):
+            del headers['Content-Encoding']
+        if headers.get('Transfer-Encoding'):
+            del headers['Transfer-Encoding']
+        headers['Content-Length'] = len(r.content)
+
+        return Response(r.content, r.status_code, headers)
+
+app.run(port = 8007, debug = True)
